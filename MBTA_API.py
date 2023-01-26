8e711da7e6fad7ea13b6d837fe71974ca7f0fb3f
jmbujold <56080126+jmbujold@users.noreply.github.com>
2023-01-26 10:32:44 -0500

    Update MBTA_API.py

--- MBTA_API.py
+++ MBTA_API.py
@@ -17,14 +17,16 @@ ct = parse(datetime.now().replace(microsecond=0).isoformat())
 # store MBTA API that returns JSON with train information for place-ccmnl stop - inbound (Community College, Orange Line, Inbound)
 url = "https://api-v3.mbta.com/predictions?filter[stop]=place-ccmnl&filter[direction_id]=0"
   
-# store the response of URL
-response = urlopen(url)
-print("RESPONSE",response)
+html = urlopen(url).read()
+print(type(html))
+data = json.loads(html.decode('utf-8'))
+print(data)
+
 
 #load JSON to variable, then get first train (0) and second train (1) arrival times. Time looks like '2023-01-25T11:57:00-05:00'
 print(response.read())
 
-data_json = json.loads(str(response, "utf-8").read())
+data_json = json.loads(str(data, "utf-8").read())
 first_train = parse(data_json['data'][0]['attributes']['arrival_time']).replace(tzinfo=None)
 second_train = parse(data_json['data'][1]['attributes']['arrival_time']).replace(tzinfo=None)
 
