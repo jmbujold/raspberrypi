a588d271352be173354e15d269ba899ea0ed1d0f
jmbujold <56080126+jmbujold@users.noreply.github.com>
2023-01-26 10:19:18 -0500

    Update MBTA_API.py

--- MBTA_API.py
+++ MBTA_API.py
@@ -23,7 +23,9 @@ print("RESPONSE",response)
 
 #load JSON to variable, then get first train (0) and second train (1) arrival times. Time looks like '2023-01-25T11:57:00-05:00'
 print(response.read())
-data_json = json.loads(response.read())
+str_content = response.decode('utf-8')
+print(str_content)
+data_json = json.loads(str_content.read())
 first_train = parse(data_json['data'][0]['attributes']['arrival_time']).replace(tzinfo=None)
 second_train = parse(data_json['data'][1]['attributes']['arrival_time']).replace(tzinfo=None)
 
