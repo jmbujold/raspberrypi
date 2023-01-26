e3edfce0d66228e2174caf4ac376a171b0a3db22
jmbujold <56080126+jmbujold@users.noreply.github.com>
2023-01-26 14:35:33 -0500

    Update display.py

--- display.py
+++ display.py
@@ -1,1 +1,18 @@
+#import LCD Driver (installed on Pi)
+import drivers
 
+from time import sleep
+
+display = drivers.Lcd()
+
+for x in range(10):
+    try:
+        display.lcd_display_string("I work", 1)  # Write line of text to first line of display
+        display.lcd_display_string("yay!", 2)  # Write line of text to second line of display
+        sleep(1)                                           # Give time for the message to be read
+        display.lcd_clear()                                # Clear the display of any data
+        sleep(1)                                           # Give time for the message to be read
+    except:
+        print("error in except")
+        display.lcd_clear()
+display.lcd_clear()
